4014233cea8383bde3a7ac4f6a0c14254c739060
Michael Nganga <ngangamichael83@gmail.com>
2022-09-27 21:15:15 +0300

    added systemchrome to block screen rotation on main function, its limited to portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,20 @@ import 'package:expense_planner/widgets/chart.dart';
 import 'package:expense_planner/widgets/new_transaction.dart';
 import 'package:expense_planner/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'models/transaction.dart';
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+    ]
+  );
+  runApp(MyApp());
+  
+}
 
 class MyApp extends StatelessWidget {
   @override
